04b5fbc16f2ffa6ce7249817cb690148ef2d467e
neBLaMontagne <benmeite23@gmail.com>
2024-04-09 14:31:46 +0200

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { TaskComponent } from './components/task/task.component';
 
 // Définition des routes
 const routes: Routes = [
   { path: '', component: LoginComponent }, // Route par défaut
+  { path: 'login', component: LoginComponent }, // Route pour la connexion
+  { path: 'signup', component: SignupComponent }, // Route pour l'inscription
+  { path: 'task', component: TaskComponent }, // Route pour l'inscription
+  { path: '**', redirectTo: '' }, // Redirection vers la page d'accueil pour les chemins non définis
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,6 @@ import { TaskService } from './services/task.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  task: Task = {}; // Initialise l'objet Task
-
-  constructor(private taskService: TaskService) {}
-  
-  addTask() {
-    if (this.task.titre && this.task.description) {
-      this.taskService.addTask(this.task).then(() => {
-        console.log('Task added successfully');
-        this.task = new Task(); // Réinitialiser le formulaire
-      });
-    }
-  }
-}
+export class AppComponent {}
